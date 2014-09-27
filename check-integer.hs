be52926bda8f1925c9caaa66401a8fad5048adfd
Erik de Castro Lopo <erikd@mega-nerd.com>
2014-09-28 09:50:00 +1000

    Remove Check/Common.hs tests.

--- check-integer.hs
+++ check-integer.hs
@@ -2,8 +2,6 @@
 
 import Test.Hspec
 
-import Check.Common
-
 import qualified Check.New1 as New1
 import qualified Check.New3 as New3
 import qualified Check.New4 as New4
@@ -11,7 +9,6 @@ import qualified Check.New4 as New4
 
 main :: IO ()
 main = hspec $ do
-    describe "Testing low level common primitives:" testCommon
     describe "Comparing GMP and New1 Integer operations:" New1.testNewInteger
     describe "Comparing GMP and New3 Integer operations:" New3.testNewInteger
     describe "Comparing GMP and New4 Integer operations:" New4.testNewInteger
